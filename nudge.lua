05d6d1a5fc596b37608477b3d2a9fb38f1e42b3f
Shlygly <Anthony.Champi.Dupont@gmail.com>
2017-02-02 14:15:56 +0100

    Add preferences setting command

--- nudge.lua
+++ nudge.lua
@@ -5,19 +5,20 @@ NUDGE_COUNT = 31 -- Nudge count to end (must be n%2 == 1)
 
 Nudge_Index = 0
 
+-- Preferences init --
 Prefs =
 {
 	hexnudge_muted=false,
 	hexnudge_menublink=true,
 	hexnudge_windowblink=false
 }
-
 for key,value in pairs(Prefs) do
 	if (hexchat.pluginprefs[key] == nil) then
 		hexchat.pluginprefs[key] = (value and "ON" or "OFF")
 	end
 end
 
+-- NUDGE & WIZZ Commands --
 hexchat.hook_command('NUDGE', function (args)
 	if (#args ~= 2) then
 		hexchat.print("Utilisation : NUDGE <pseudo>")
@@ -36,12 +37,31 @@ hexchat.hook_command('WIZZ', function (args)
 	end
 end)
 
+-- Preferences Settings Command --
+hexchat.hook_command('WZSET', function (args)
+	if (#args == 1) then
+		for key,value in pairs(hexchat.pluginprefs) do
+			hexchat.print(key.." = "..value)
+		end
+	elseif (#args == 3) then
+		if (hexchat.pluginprefs[args[2]] ~= nil) then
+			hexchat.pluginprefs[args[2]] = args[3]
+		else
+			hexchat.print("Variable inconnu")
+		end
+	else
+		hexchat.print("Utilisation : WZSET [<variable> <valeur>]")
+	end
+end)
+
+-- NUDGE & WIZZ Reception --
 hexchat.hook_server('NOTICE', function (args, args_eol)
 	if (args_eol[4] == ':WIZZ') then
 		DoNudge()
 	end
 end)
 
+-- Do a Nudge --
 function DoNudge()
 	if (not GetPref("hexnudge_muted") and Nudge_Index <= 0) then
 		Nudge_Index = 0
@@ -68,6 +88,7 @@ function DoNudge()
 	end
 end
 
+-- Utilities --
 function GetPref(pref)
 	if (hexchat.pluginprefs[pref] == "ON") then
 		return true
